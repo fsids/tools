4dcd722a068f17f22edc4ddb096149ebad43529b
fsids <fsids3@gmail.com>
2017-07-26 12:30:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function redirect() {
-    var textValue = document.getElementById("pocket").value;
-    window.open("https://getpocket.com/a/queue/grid/" + textValue;
-
-}
-                
+           
 function openTab(url) {
    var url = "https://getpocket.com/a/queue/grid/" + url;
    var win = window.open(url, '_blank');
