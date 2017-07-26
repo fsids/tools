f3243fba95e12ef3ace147ef68e0ac4143acb54d
fsids <fsids3@gmail.com>
2017-07-26 12:25:01 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,9 @@ function redirect() {
     window.open("https://getpocket.com/a/queue/grid/" + textValue;
 
 }
+                
+function openTab(url) {
+   var url = "https://getpocket.com/a/queue/grid/" + url;
+   var win = window.open(url, '_blank');
+   win.focus();
+}
